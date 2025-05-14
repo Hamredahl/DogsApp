daafe312f8d5ba26c068a16666636872cf782054
Hamredahl <JHamredahl@gmail.com>
2025-05-14 11:44:50 +0200

    Initial commit + Dog model

---
+++ DogsApp.Mvc/Models/Dog.cs
@@ -0,0 +1,9 @@
+﻿namespace DogsApp.Mvc.Models
+{
+    public class Dog
+    {
+        int Id {  get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+    }
+}
