964b7f3a2ecfabcdc32c5d695656aa4439d09801
Hamredahl <JHamredahl@gmail.com>
2025-05-14 11:58:24 +0200

    Added DogsController functionality

---
+++ DogsApp.Mvc/Controllers/DogsController.cs
@@ -0,0 +1,18 @@
+﻿using DogsApp.Mvc.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogsApp.Mvc.Controllers;
+
+public class DogsController : Controller
+{
+    static DogService dogService = new DogService();
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        var model = dogService.GetAllDogs();
+        return View(model);
+    }
+
+
+}
