029bc0ffb36c193c817f0f82892cc9a03f944f21
Hamredahl <JHamredahl@gmail.com>
2025-05-14 11:51:45 +0200

    added DogService-class

--- DogsApp.Mvc/Models/Dog.cs
+++ DogsApp.Mvc/Models/Dog.cs
@@ -2,7 +2,7 @@
 {
     public class Dog
     {
-        int Id {  get; set; }
+        public int Id {  get; set; }
         public string Name { get; set; }
         public string Age { get; set; }
     }
---
+++ DogsApp.Mvc/Services/DogService.cs
@@ -0,0 +1,26 @@
+﻿using DogsApp.Mvc.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DogsApp.Mvc.Services;
+
+public class DogService
+{
+    private List<Dog> dogs = [
+
+        ];
+
+
+    public void AddDog(Dog dog)
+        {
+            dogs.Add(dog);
+        }
+
+    public Dog[] GetAllDogs() => dogs
+        .OrderBy(d => d.Name)
+        .ToArray();
+    
+
+    public Dog? GetDogById(int Id) => dogs
+        .SingleOrDefault(d => d.Id == Id);
+    
+}
